536d6dcebc1d95cdb621d68bfa52b9baa7bfdcda
StarGrim33 <prosky33@gmail.com>
2022-10-25 20:16:51 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace HomeWorkCollections3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            bool isProgramOn = true;
+            string? userInput = null;
+            string sumCommand = "sum";
+            string exitCommand = "exit";
+
+            while (isProgramOn)
+            {
+                Console.WriteLine("Введите число: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == sumCommand)
+                {
+                    int sum = 0;
+
+                    foreach (int number in numbers)
+                    {
+                        sum += number;
+                    }
+                    Console.WriteLine("Сумма чисел равна: " + sum);
+                }
+                else if (userInput == exitCommand)
+                {
+                    isProgramOn = false;
+                    Console.Clear();
+                    Console.SetCursorPosition(50, 0);
+                    Console.WriteLine("До свидания!");
+                }
+                else
+                {
+                    int convertStringToNumber = 0;
+
+                    if (int.TryParse(userInput, out convertStringToNumber))
+                    {
+                        numbers.Add(convertStringToNumber);
+                    }
+                }
+            }
+        }
+
+    }
+}
